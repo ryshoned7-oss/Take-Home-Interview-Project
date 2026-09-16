df3d2c3aa4d23a6b27f6f4283b35b152977a9f99
Ryshone Duncan <ryshond7@gmail.com>
2026-09-16 16:26:17 -0400

    In progress of API and Java/finishing wrapper for movie posters

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 //API: https://www.omdbapi.com/?i=tt3896198&apikey=f6b1657f&s=fast
 
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('movies-list');
+
+  function renderMovie(movie) {
+    const el = document.createElement('div');
+    el.className = 'movie';
+    el.innerHTML = `
+      <h4>${escapeHtml(movie.title)}</h4>
+      <p>${escapeHtml(movie.overview || '')}<p>
+    `;
+    return el
+  }
+})
